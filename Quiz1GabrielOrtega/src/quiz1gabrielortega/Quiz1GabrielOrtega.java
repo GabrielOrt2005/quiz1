ded024a4a3a27d950cf6bf4a01906254c84ddd35
GabrielOrt2005 <gaortsal4@gmail.com>
2024-06-19 20:48:59 -0600

    Add files via upload

---
+++ Quiz1GabrielOrtega/src/quiz1gabrielortega/Quiz1GabrielOrtega.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package quiz1gabrielortega;
+
+import javax.swing.JOptionPane;
+
+public class Quiz1GabrielOrtega {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int cantidadEmpleados;
+        String Suma;
+        String salarioEmpleados;
+        double salariototal;
+        int suma=0; 
+        
+        String EmpleadosCa;
+        EmpleadosCa = JOptionPane.showInputDialog("Digite la cantidad de empleados:");
+        for (int i = 0; i < 10; i++) {
+           salarioEmpleados = JOptionPane.showInputDialog("Ingrese los salarios:");
+           Suma = salarioEmpleados+suma;
+           
+            String SalarioF = JOptionPane.showInputDialog("El salario final es:"+ Suma);
+            
+              
+        }
+            
+        }
+       
+    }
+    
+
